ddef7314a6ec8f3c9f5f832f7f01e18f41c16386
douglasd11 <dguerreroe@unicartagena.edu.co>
2023-05-01 17:50:41 -0500

    portada agregada al login

--- cliente/src/components/Login.js
+++ cliente/src/components/Login.js
@@ -2,22 +2,27 @@ import React from 'react'
 
 function Login(){
     return(
-        <div className='container col-4 mt-5'>
-            <h2 className='text-center'>Iniciar Sesion</h2>
-            <div className='mt-5'>
-                <div className="mb-3">
-                    <label className="form-label">Correo electronico</label>
-                    <input type="email" className="form-control" placeholder='Correo electronico' id="exampleInputEmail1"/>
+        <div className='container row col-12 mt-5'>
+            <div className='col-5 me-5'>
+                <h2 className='text-center mt-5'>Iniciar Sesion</h2>
+                <div className='mt-5'>
+                    <div className="mb-3">
+                        <label className="form-label">Correo electronico</label>
+                        <input type="email" className="form-control" placeholder='Correo electronico' id="exampleInputEmail1"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Contraseña</label>
+                        <input type="password" className="form-control" placeholder='Contraseña' id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                        <label className="form-check-label" for="exampleCheck1">Recordarme</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary mt-3">Iniciar sesion</button>
                 </div>
-                <div className="mb-3">
-                    <label className="form-label">Contraseña</label>
-                    <input type="password" className="form-control" placeholder='Contraseña' id="exampleInputPassword1"/>
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" for="exampleCheck1">Recordarme</label>
-                </div>
-                <button type="submit" className="btn btn-primary mt-3">Iniciar sesion</button>
+            </div>
+            <div className='col-6'>
+                <img className='w-100' src='/portada.png'></img>
             </div>
         </div>
     )
